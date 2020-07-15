dd7c0870b4d16e67401831eaa36e3f27c7fac40e
Alexander Sturm <al.stu@gmx.de>
2020-07-15 19:54:25 +0200

    LINQReadFromCSV Added

---
+++ LINQCSVRead/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace LINQCSVRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\alstu\Downloads\zuordnung_plz_ort.csv";
+            string[] plzFile = File.ReadAllLines(path);
+
+            var query = from csvLine in plzFile let valueString = csvLine.Split(",") select new { id = valueString[0], ort=valueString[1]  ,plz = valueString[2], bundesland = valueString[3]};
+
+            var list = query.ToList();
+
+            foreach (var dataLine in list) {
+                Console.Write($"{dataLine.id} / {dataLine.ort} / {dataLine.plz} / {dataLine.bundesland}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
